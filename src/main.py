041b16e655f33f788a6555457c9bb9945f1e8448
Pareexit-Baldaniya-Innvonix <pareexit.baldaniya@innvonix.in>
2026-03-12 14:26:39 +0530

    modify file and remove the rate limit status code

--- src/main.py
+++ src/main.py
@@ -1,43 +1,42 @@
-from fastapi import FastAPI, Request, HTTPException
-from classes.FixedWindowCounter import FixedWindowCounter
-import uvicorn
 import time
+import uvicorn
 from Settings import db_settings
+from fastapi import FastAPI, Request
+from classes.StatusCode import StatusCode
+from classes.FixedWindowCounter import FixedWindowCounter
 
 app = FastAPI()
 
 limiter = FixedWindowCounter(limit=db_settings.REQUEST_PER_SECOND, seconds=1)
 
 
-@app.get("/")
+@app.get("/")  # Base API route
 def read_root():
     return {"message": "Welcome to FastAPI tutorials!"}
 
 
-@app.get("/hello")
-def greet(request: Request):
-    client_ip = request.client.host
-
-    # check actual clients IP
-    if not limiter.allow_request(client_ip):
-        raise HTTPException(status_code=429, details="Too many requests")
-
-    return {"hello": "world", "ip": client_ip}
-
-
-def main():
+def run_test():
     user_ip = limiter.get_ip()
     print(f"Detected IP: {user_ip}")
 
     for i in range(25):
         allowed = limiter.allow_request(user_ip)
-        status = "Allowed" if allowed else "Denied (Error! 429 Too Many Requests)"
-        print(f"Request {i+1}: {status}")
+        if allowed:
+            print(f"Request {i+1}: Allowed")
+        else:
+            print(f"Request {i+1}: Denied", StatusCode.error_code())
         time.sleep(1)
 
 
+@app.get("/hello")  # API route
+def greet(request: Request):
+    client_ip = request.client.host
+
+    return {"hello": "world", "ip": client_ip}
+
+
 if __name__ == "__main__":
-    main()
+    run_test()
 
     print("\n--- Starting FastAPI server ---")
-    uvicorn.run(app, host="0.0.0.0", port=8001)
+    uvicorn.run("main:app", host="0.0.0.0", port=8001, reload=True)
