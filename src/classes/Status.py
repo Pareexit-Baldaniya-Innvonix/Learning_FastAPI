a8898feabb23cfc9a971c143f911f1b682d61c06
Pareexit-Baldaniya-Innvonix <pareexit.baldaniya@innvonix.in>
2026-03-13 16:18:56 +0530

    Error code for too many requests

---
+++ src/classes/Status.py
@@ -0,0 +1,11 @@
+from fastapi import HTTPException
+
+
+class Status:
+
+    @staticmethod
+    def raise_rate_limit_error():
+        raise HTTPException(
+            status_code=429,
+            detail="Too many requests",
+        )
