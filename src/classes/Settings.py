d0d795171f9bef05231913fbb69bcd56650eef1d
Pareexit-Baldaniya-Innvonix <pareexit.baldaniya@innvonix.in>
2026-03-16 17:05:28 +0530

    update the file

---
+++ src/classes/Settings.py
@@ -0,0 +1,10 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(validate_default=False)
+
+    REQUEST_PER_SECOND: int = 20
+
+
+settings = Settings()
