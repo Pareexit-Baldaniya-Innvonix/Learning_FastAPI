13bf3c370c7fa472d7f653bd8bb51ca4cac8b331
Pareexit-Baldaniya-Innvonix <pareexit.baldaniya@innvonix.in>
2026-03-20 12:55:00 +0530

    create logging module for checking all logs from the project

---
+++ src/utils/logger.py
@@ -0,0 +1,87 @@
+# ----- library import -----
+import logging
+from logging.config import dictConfig
+
+# ----- local import -----
+from src.classes.Settings import settings
+from src.config.constants import LOG_APP
+
+# unified format (same as logging module)
+_LOG_FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+_DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
+
+# mapping levels from environment variables to logging constants
+LEVEL_MAP = {
+    "DEBUG": logging.DEBUG,
+    "INFO": logging.INFO,
+    "WARNING": logging.WARNING,
+    "ERROR": logging.ERROR,
+    "CRITICAL": logging.CRITICAL,
+}
+
+# determine log level based on settings, default is INFO
+TARGET_LEVEL = LEVEL_MAP.get(settings.LOG_LEVEL, logging.INFO)
+
+# centralized logging configuration dictionary
+LOGGING_CONFIG = {
+    "version": 1,
+    "disable_existing_loggers": False,  # keeps default loggers active so we can override them
+    "formatters": {
+        "default": {
+            "format": _LOG_FORMAT,
+            "datefmt": _DATE_FORMAT,
+        },
+    },
+    "handlers": {
+        # printing logs to the terminal
+        "console": {
+            "formatter": "default",
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stdout",  # send logs to terminal
+        },
+        # saving logs in an app.log file with 5MB limit and rotation
+        "file": {
+            "formatter": "default",
+            "class": "logging.handlers.RotatingFileHandler",
+            "filename": LOG_APP,
+            "maxBytes": 5 * 1024 * 1024,  # 5MB
+            "backupCount": 3,  # Keep 3 old log files
+            "encoding": "utf8",
+        },
+    },
+    "loggers": {
+        # redirect Uvicorn general logs to custom handlers
+        "uvicorn": {
+            "handlers": ["console", "file"],
+            "level": TARGET_LEVEL,
+            "propagate": False,
+        },
+        # redirect Uvicorn error logs
+        "uvicorn.error": {
+            "level": TARGET_LEVEL,
+            "handlers": ["console", "file"],
+            "propagate": False,
+        },
+        # redirect Uvicorn access logs
+        "uvicorn.access": {
+            "handlers": ["console", "file"],
+            "level": "WARNING",  # using WARNING for preventing other logs
+            "propagate": False,
+        },
+    },
+    "root": {
+        # default logger for the rest of the application
+        "handlers": ["console", "file"],
+        "level": TARGET_LEVEL,
+    },
+}
+
+
+# applies dictionary to the entire project
+def setup_logging():
+    dictConfig(LOGGING_CONFIG)
+
+
+# returns a logger instance for a specific module
+def get_logger(name: str):
+    return logging.getLogger(name)
