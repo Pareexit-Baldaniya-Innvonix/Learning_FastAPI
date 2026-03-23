63c98ec492cf61f62867a7225e89817042dd151d
Pareexit-Baldaniya-Innvonix <pareexit.baldaniya@innvonix.in>
2026-03-23 15:25:15 +0530

    created a middleware for '/hello' route

---
+++ src/classes/Middleware.py
@@ -0,0 +1,22 @@
+# ----- library import -----
+from fastapi import Request
+from starlette.middleware.base import BaseHTTPMiddleware
+
+# ----- local import -----
+from src.utils.logger import get_logger
+
+logger = get_logger(__name__)
+
+
+class LoggingMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next):
+        client_ip = request.client.host if request.client else "unknown"
+
+        response = await call_next(request)
+
+        # check for route is "/hello"
+        if request.url.path == "/hello":
+            logger.info(
+                f"client_ip: {client_ip} visits {request.method} {request.url.path} status {response.status_code}"
+            )
+        return response
